22cfeedbb8e93d766837e6ba60305267d1b3749e
Kulsum Kamal <82769080+kulsumkamal@users.noreply.github.com>
2022-06-15 16:02:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ function stopChaos() {
     image.style.animationPlayState = "paused";
     document.getElementById("Sub-form").style.display="block";
 }
+
+function startChaos() {
+    const cancel = document.getElementById('Sub-form');
+    cancel.style.display='none';
+    const vid = document.getElementById("bgvid");
+    const image = document.getElementById("doom_image");
+    vid.play();
+    image.style.animationPlayState = "running";
+}
+
+function login() {
+    const btn = document.getElementsByName("password-rpt");
+    btn[0].style.display = 'none';
+    btn[1].style.display = 'none';
+}
+
+function subscribe() {
+    const btn = document.getElementsByName("password-rpt");
+    btn[0].style.display = 'block';
+    btn[1].style.display = 'block';
+}
